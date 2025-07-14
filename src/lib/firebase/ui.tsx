d5732380123ce7187f1016ba36d28bb5fd87a2f9
Adrian Cruz <kangoonie@gmail.com>
2025-07-14 09:14:39 +0200

    refactor: remove user icon from login button

--- src/lib/firebase/ui.tsx
+++ src/lib/firebase/ui.tsx
@@ -1,12 +1,11 @@
 'use client'
-import { TbUserFilled } from 'react-icons/tb'
 import { useUser } from './hooks'
 
 export function LoginButton() {
   const { signIn } = useUser()
   return (
     <button className='btn btn-primary rounded-4xl' onClick={signIn}>
-      <TbUserFilled size={18} /> Sign in
+      Sign in
     </button>
   )
 }
